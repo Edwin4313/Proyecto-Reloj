92db82de50cf9666794b9566d92c8c78433254ab
Edev <caizadavid417@gmail.com>
2023-02-11 13:15:20 -0500

    Reloj Medio - 13:08

---
+++ clock.js
@@ -0,0 +1,10 @@
+let hr = document.querySelector('#hr')
+let mn = document.querySelector('#mn')
+let sc = document.querySelector('#sc')
+setInterval(() =>{
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+    hr.getElementsByClassName.apply();
+})
